b30309514e3b197ea7acd6e8738002c020dbaf56
Eric Feldman <eric.feldman@rotellacapital.com>
2024-05-29 14:34:31 -0700

    dynamic prompt testing

--- index.ts
+++ index.ts
@@ -8,18 +8,22 @@ dotenv.config();
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
-const chatModel = new ChatOpenAI({ apiKey: process.env.OPEN_AI_API_KEY });
-
-app.get("/", async (req: Request, res: Response) => {
-  const output = await test();
-  res.json({ response: output });
+const chatModel = new ChatOpenAI({
+  apiKey: process.env.OPEN_AI_API_KEY,
+  model: "gpt-3.5-turbo-0125",
 });
 
-const test = async () => {
+app.get("/destination", async (req: Request, res: Response) => {
+  const loc: string = req.query.location as string;
+  console.log(`Request destination: ${loc}`);
+  const output = await test(loc);
+  res.json(output);
+});
+
+const test = async (location: string) => {
   return await chatModel
     .invoke(
-      `
-  I want to go on a vacation but I do not know where I want to go yet. I am located in Seattle, WA. I am thinking of an international trip. I want to go during the month of June. Can you recommend 10 places for me to go? Can you return this as a json object, the location should have the key "location" and then why part should have the key "why".`
+      `I want to go on a vacation but I do not know where I want to go yet. I am located in ${location}. I am thinking of doing a local road trip. I want this road trip to not be longer than 4 hours away. I want to go during the month of June. Can you recommend 3 places for me to go? Can you return this as a json object, the location should have the key "location" and then why part should have the key "why".`
     )
     .then((out) => JSON.parse(out.content as string))
     .catch((e) => console.log(e.message));
