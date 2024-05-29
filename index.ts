3e4c31fde7e14fdff058b023b0c03201b81a98db
Eric Feldman <eric.feldman@rotellacapital.com>
2024-05-29 13:34:56 -0700

    adds langchain init

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response, Application } from "express";
 import dotenv from "dotenv";
+import { ChatOpenAI } from "@langchain/openai";
 
 //For env File
 dotenv.config();
@@ -7,10 +8,16 @@ dotenv.config();
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
+const chatModel = new ChatOpenAI({ apiKey: process.env.OPEN_AI_API_KEY });
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Init Test Server");
+  res.send(test());
 });
 
+const test = async () => {
+  return await chatModel.invoke("Hello!").catch((e) => console.log(e.message));
+};
+
 app.listen(port, () => {
-  console.log(`Server is live at http://localhost:${port}`);
+  console.log(test());
 });
