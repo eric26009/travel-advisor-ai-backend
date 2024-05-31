db18182d9fb0d989d943b9c727525f82cae94313
Eric Feldman <eric.feldman@rotellacapital.com>
2024-05-31 16:54:28 -0700

    cors whitelists localhost:3000 for dev work

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 import express, { Request, Response, Application } from 'express';
 import dotenv from 'dotenv';
 import { unknownDestinationRouter } from './routes';
+import cors from 'cors';
 
 //For env File
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
-const cors = require('cors');
 
-app.use(cors({ origin: '*' }));
+app.use(
+  cors({
+    credentials: true,
+    origin: ['http://localhost:3000'] // Whitelist the domains you want to allow
+  })
+);
 app.use('/travel', unknownDestinationRouter);
 
 app.get('/', async (req: Request, res: Response) => {
